ae3d8498198f795c1bd98281be70ff4ac108c310
mumblejumble <danqing.lin@spsmail.cuny.edu>
2015-09-26 01:14:35 +0000

    task_02 added (outside of task_01 package)

---
+++ task_02.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Module Task_02"""
+
+
+import task_01.peanut
+TIME = task_01.peanut.BUTTER
+"""Assigning BUTTER's value from peanut module in task_01 package to TIME"""
